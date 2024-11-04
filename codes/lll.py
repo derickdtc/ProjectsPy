215df7430e60545fd0eb2f9195be86e2435caa72
derickdtc <dericktchagas04@gmail.com>
2024-11-04 10:37:25 -0300

    Create lll.py

---
+++ codes/lll.py
@@ -0,0 +1,9 @@
+def calcular_area_triangulo(base, altura):
+    return (base * altura) / 2
+
+# Exemplo de uso
+base = float(input("Digite a base do triângulo: "))
+altura = float(input("Digite a altura do triângulo: "))
+
+area = calcular_area_triangulo(base, altura)
+print(f"A área do triângulo é: {area}")
